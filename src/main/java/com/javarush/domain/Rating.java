6739bceb79d3411188ae621b4e3b0ff010d9aa76
HellEnjoy <101444928+HellEnjoy@users.noreply.github.com>
2023-01-24 10:58:35 +0600

    Initial commit

---
+++ src/main/java/com/javarush/domain/Rating.java
@@ -0,0 +1,21 @@
+package com.javarush.domain;
+
+public enum Rating {
+    G("G"),
+    PG("PG"),
+    PG13("PG-13"),
+    R("R"),
+    NC17("NC-17");
+
+    private final String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    Rating(String value) {
+        this.value = value;
+
+
+    }
+}
